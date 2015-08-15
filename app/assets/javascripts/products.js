e34fde717644446460217b265e122762047982da
Alex Kutuzaki <alex.ktzk@gmail.com>
2015-08-15 19:14:27 +0300

    Few renderrer added

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,2 +1,14 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+document.ready(function() {
+    var vat_included_price,
+        price,
+        vat;
+
+    price = $("#price");
+    vat = $("#vat").html();
+    vat_included_price = $("#vat_included_price");
+
+    price.keyup(function() {
+        vat_included_price.html(this.html() * vat);
+    });
+});
+
